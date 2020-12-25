b54c2a43ea5cdb71fa0d58288204dcb82d5945ce
Tom Conley <tommyc38@gmail.com>
2020-12-24 21:58:27 -0700

    initial commit

---
+++ apps/umbrelo/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'umbrelo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'umbrelo-main';
+}
